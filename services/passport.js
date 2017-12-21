e2e9efbb7fef50ab90b8b6f895e2495948d5267d
akari0624 <akari0624@hotmail.com>
2017-12-21 18:56:37 +0800

    install passport

---
+++ services/passport.js
@@ -0,0 +1,6 @@
+const passport = require('passport');
+const UserModel = require('../models/user');
+const config = require('../config');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
