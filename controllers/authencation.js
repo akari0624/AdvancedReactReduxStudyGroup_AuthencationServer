5e48b976d6ea2dbe1b573f479fda732087223c1e
akari0624 <akari0624@hotmail.com>
2017-12-26 00:03:21 +0800

    do mongoose findOne,save in promise way after mongoose 4.0

--- controllers/authencation.js
+++ controllers/authencation.js
@@ -1,58 +1,54 @@
-const UserModel = require('../models/user');
-const config = require('../config');
-const jwt = require('jwt-simple');
+const UserModel = require("../models/user");
+const config = require("../config");
+const jwt = require("jwt-simple");
 
-
-function tokenForUser(user){
-    const timeStamp = new Date().getTime();
-    return jwt.encode({ sub: user.id,
-                        iat: timeStamp}, config.secrect);
+function tokenForUser(user) {
+  const timeStamp = new Date().getTime();
+  return jwt.encode(
+    {
+      sub: user.id,
+      iat: timeStamp
+    },
+    config.secrect
+  );
 }
 
-exports.signup = function(req, res, next){
-  
-    // See if a user with the given email exists
-     const email = req.body.email;
-     const password = req.body.password;
-     
-    if(!email || !password){
+exports.signup = function(req, res, next) {
+  // See if a user with the given email exists
+  const email = req.body.email;
+  const password = req.body.password;
 
-        return res.status(422).send({error:'please provide email and password'});
-    }
+  if (!email || !password) {
+    return res.status(422).send({ error: "please provide email and password" });
+  }
 
-         
-    UserModel.findOne({email:email},(err,existUser)=>{
 
-        if(err){
-            return next(err);
-        }
-  
-    // if a user with that email does exist, return an error
-        if(existUser){
+  UserModel.findOne({ email: email })
+    .then(existUser => {
+      // if a user with that email does exist, return an error
+      if (existUser) {
+        return res.status(422).send({ error: "email already in use..." });
+      }
 
-            return res.status(422).send({error:'email already in use...'});
-        }
-    
-    // If a user with that email does NOT exist, create and save user record
-    const user = new UserModel(
-        {
-            email:email,
-            password:password
-        }
-    );
+      // If a user with that email does NOT exist, create and save user record
+      const user = new UserModel({
+        email: email,
+        password: password
+      });
 
-    user.save((err)=>{
-
-        if(err){
-            return next(err);
-        }
-
-// Respond to response  to indicating the user was created
-        res.json({token: tokenForUser(user)});
+      user
+        .save()
+        .then(
+          // Respond to response  to indicating the user was created
+          res.json({ token: tokenForUser(user) })
+        )
+        .catch(err => {
+          return next(err);
+        });
+    })
+    .catch(err => {
+      if (err) {
+        return next(err);
+      }
     });
-    
-
-
-   });
-
-}
+};
