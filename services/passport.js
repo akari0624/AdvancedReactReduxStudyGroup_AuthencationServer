e8cd1f5bf9b401824d59ef28a94220f5406913b6
akari0624 <akari0624@hotmail.com>
2017-12-27 00:07:58 +0800

    implements the excuting callback when jwt is not fake

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,29 @@
-const passport = require('passport');
-const UserModel = require('../models/user');
-const config = require('../config');
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
+const passport = require("passport");
+const UserModel = require("../models/user");
+const config = require("../config");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
 
+const jwtOptions = {};
+
+// create jwt strategy
+const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
+  //see if the user ID in the payload exists in our database
+  // if it does, call 'done' with that other
+  //otherwise, call done without a user object
+
+  const id = payload.sub;
+  UserModel.findOne(id)
+    .then(user => {
+      if (user) {
+        return done(null, user);
+      }
+
+      return done(null, false);
+    })
+    .catch(err => {
+      return done(err, false);
+    });
+});
+
+// tell passport to use this strategy
