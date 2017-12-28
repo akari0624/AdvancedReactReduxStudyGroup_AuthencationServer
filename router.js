bcc19623d006d84f6aaf32b50ec517b2d92af898
akari0624 <akari0624@hotmail.com>
2017-12-28 18:29:41 +0800

    mapping the  route go through passportLocal middleware

--- router.js
+++ router.js
@@ -1,10 +1,11 @@
 const Authencation = require("./controllers/authencation");
 const PassportAuth = require("./services/passport");
-
+const PassportLocal = require("./services/passport-local");
 
 module.exports = function(app) {
   app.get("/", PassportAuth.requireJWTAuth, (req, res) => {
     res.send({ hi: "there" });
   });
   app.post("/signup", Authencation.signup);
+  app.post("/signin", PassportLocal.localSignInAuth, Authencation.signIn);
 };
